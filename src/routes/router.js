df77198bb4ab1fffd61882e5970c8996d84c4488
Jonathan César <jonathanjog237@gmail.com>
2023-02-23 17:52:27 -0300

    Criando e Organizando rotas da aplicação

--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,10 @@ const { Router } = require("express")
 const router = Router()
 const controller = require("../controllers/controller")
 
-
+router.post('/post', controller.Create)
+router.get('/', controller.Read)
+router.get('/post', controller.ReadOne)
+router.put('/post', controller.Update)
+router.delete('/post', controller.Delete)
 
 module.exports = router
